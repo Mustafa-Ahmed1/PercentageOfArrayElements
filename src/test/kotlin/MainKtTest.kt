74dcdb7ff3b68a7e964437ff9d0d39876e16ffaf
Mustafa A. Muhammed <42946875+Musty010@users.noreply.github.com>
2022-05-22 00:05:06 +0300

    Update MainKtTest.kt

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -3,12 +3,43 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class MainKtTest {
-
     @Test
-    fun main() {
+    fun should_ReturnPercentageOfA_When_ArrayElementsAreWrittenCorrectly() {
+        // given an array with only "a" and "b" characters
+        val array = arrayOf("a", "a", "b", "a", "b", "a")
+        // when calculate the percentage of "a"
+        val percentageOfA = findPercentageOfa(array)
+        // then check the result
+        assertEquals(66.7, percentageOfA)
     }
 
     @Test
-    fun findPercentageOfa() {
+    fun should_ReturnMinus1_When_SomeArrayElementsContainMoreThan1Character() {
+        // given an array with more than one character in some of its strings
+        val array = arrayOf("ab", "aaa", "b")
+        // when try to calculate the percentage of "a"
+        val percentageOfA = findPercentageOfa(array)
+        // then check the result
+        assertEquals(-1, percentageOfA)
+    }
+
+    @Test
+    fun should_ReturnMinus1_When_SomeArrayElementsAreNotAsNorBs() {
+        // given an array with different types of characters that some of them are not "a" nor "b"
+        val array = arrayOf("m", "1", "*", "", "b")
+        // when try to calculate the percentage of "a"
+        val percentageOfA = findPercentageOfa(array)
+        // then check the result
+        assertEquals(-1, percentageOfA)
+    }
+
+    @Test
+    fun should_ReturnMinus1_When_ThereIsNoCharacterInArray() {
+        // given an array with empty strings
+        val array = arrayOf("")
+        // when try to calculate the percentage of "a"
+        val percentageOfA = findPercentageOfa(array)
+        // then check the result
+        assertEquals(-1, percentageOfA)
     }
 }
