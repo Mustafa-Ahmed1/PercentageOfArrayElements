97119e0c607ee2f9ac73ded686620d5e90b14d16
Mustafa <mstrfyjmalalden@gmail.com>
2022-05-21 21:22:16 +0300

    Initial commit

---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainKtTest {
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun findPercentageOfa() {
+    }
+}
